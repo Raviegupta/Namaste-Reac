aacd93c11913ee9555c6e70a89e0f9c496f5b473
Raviegupta <raviegupta153@gmail.com>
2023-12-28 20:36:33 +0530

    react ch 1: Nested React Element

--- App.js
+++ App.js
@@ -1,5 +1,24 @@
+/* 
+<div id="parent">
+    <div id="child">
+        <h1>Hello, Its a Nested structure</h1>
+    </div>
+</div> 
+*/
+
+const parent = React.createElement(
+    'div', 
+    {id: 'parent'}, 
+    React.createElement(
+        'div', 
+        {id: 'child'}, 
+        React.createElement('h2', {id: ''}, 'Hello, Its a Nested structure'))
+    )
+
+
+
 const heading = React.createElement("h1", {id: 'heading', xyz:'abc'}, "Hello World from React");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
 
 console.log(heading); // It is an Object. hence, React.createElement returns an Object
